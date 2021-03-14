b6dfb1d49e1d5d5985fda12a4e8bb8ad3eedd67c
Anton Davydov <antondavydov.o@gmail.com>
2021-03-14 23:24:30 +0300

    Fix repository (auth and schema registry)

--- auth/config/initializers/doorkeeper.rb
+++ auth/config/initializers/doorkeeper.rb
@@ -10,7 +10,7 @@ Doorkeeper.configure do
     # raise "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
     # Put your resource owner authentication logic here.
     # Example implementation:
-    Account.find_by(id: session['warden.user.account.key'].first.first) || redirect_to(new_account_session_path)
+    Account.find_by(id: session['warden.user.account.key']&.first&.first) || redirect_to(new_account_session_path)
   end
 
   # If you didn't skip applications controller from Doorkeeper routes in your application routes.rb
