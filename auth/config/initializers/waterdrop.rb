4e1375eb3b7872d6a86e29c79d30ed579014e1b3
Anton Davydov <antondavydov.o@gmail.com>
2022-05-04 21:00:35 +0300

    Rollback kafka url config for auth service

--- auth/config/initializers/waterdrop.rb
+++ auth/config/initializers/waterdrop.rb
@@ -3,6 +3,6 @@ require 'water_drop/sync_producer'
 
 WaterDrop.setup do |config|
   config.deliver = true
-  # config.kafka.seed_brokers = [ENV['KAFKA_URL']]
-  config.kafka.seed_brokers = ['kafka://198.163.0.68:9092']
+  config.kafka.seed_brokers = [ENV['KAFKA_URL']]
+  # config.kafka.seed_brokers = ['kafka://198.163.0.68:9092']
 end
